0c36a18e890694f3cc84d9936765a68cd9dcf929
Phosphophllite <2891740789@qq.com>
2025-04-28 22:54:38 +0800

    1.突然发现为什么我的小程序端没有选择规格的菜品，因为我的DishMapper.findByCategoryId()查询的时候并没有与菜品味道表联查 2.目前用户端根据分类id查找所有的套餐其实是有bug的，因为在用户端中未起售的套餐也在用户端中显示了 3.通过dishId查询DishVO的方法无法返回flavors数据，初步判断是我的动态sql语句有问题

--- sky-server/src/main/java/com/sky/mapper/ShoppingCartMapping.java
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapping.java
@@ -3,16 +3,22 @@ package com.sky.mapper;
 import com.sky.entity.ShoppingCart;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface ShoppingCartMapping {
-    @Insert("insert into shopping_cart(name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time)" +
-            "VALUES(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor}, #{amount},#{createTime})")
+//    @Insert("insert into shopping_cart(name, image, user_id, dish_id, setmeal_id, dish_flavor, amount, create_time)" +
+//            "VALUES(#{name},#{image},#{userId},#{dishId},#{setmealId},#{dishFlavor}, #{amount},#{createTime})")
+//    void addShoppingCart(ShoppingCart shoppingCart);
+//    void updateShoppingCart(ShoppingCart updateShoppingCart);
+//    ShoppingCart findDataById(@Param("setmealId") Long setmealId, @Param("dishId")Long dishId,@Param("userId") Long userId);
+
+    List<ShoppingCart> findCart(ShoppingCart shoppingCart);
+
+    void updateCart(ShoppingCart cart);
+
     void addShoppingCart(ShoppingCart shoppingCart);
-
-    void updateShoppingCart(ShoppingCart updateShoppingCart);
-
-
-    ShoppingCart findDataById(Long setmealID,Long dishId, Long userId);
 }
--- sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -19,6 +19,7 @@ import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -31,77 +32,119 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     @Resource
     private SetmealMapper setmealMapper;
 
-    // NOTICE
     @Override
     @Transactional
     public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
-        // 现构建好购物车对象
         ShoppingCart shoppingCart = ShoppingCart.builder().build();
-        // 先将userId赋值给shoppingCart
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
         Long userId = BaseContext.getCurrentId().get(JwtClaimsConstant.USER_ID);
         shoppingCart.setUserId(userId);
-        // 判断添加进购物车的对象是Setmeal还是Dish,由此获取Setmeal还是Dish
-        if(shoppingCartDTO.getSetmealId() != null && shoppingCartDTO.getSetmealId() > 0) {
-            // select:判断该用户是否是第一次添加，如果是同样的商品，update:只需要修改购物车中的商品，将数量加一以及修改价格
-            SetmealVO setmealVO = SetmealVO.builder().build();
-            ShoppingCart updateShoppingCart = ShoppingCart.builder().build();
-            if( (updateShoppingCart = shoppingCartMapping.findDataById(shoppingCartDTO.getSetmealId(),null,userId)) != null){
-                // 商品数量
-                Integer setmealNumber = updateShoppingCart.getNumber();
-                // 商品总价
-                BigDecimal amount = updateShoppingCart.getAmount();
-                // 商品单价
-                BigDecimal setmealSinglePrice = amount.divide(BigDecimal.valueOf(setmealNumber),2, RoundingMode.HALF_UP);
-                updateShoppingCart.setNumber(setmealNumber + 1);
-                updateShoppingCart.setUserId(userId);
-                updateShoppingCart.setAmount(setmealSinglePrice.multiply(BigDecimal.valueOf(setmealNumber)));
-                // 需要修改数量和该套餐在购物车中总价
-                shoppingCartMapping.updateShoppingCart(updateShoppingCart);
-                // 对数据库的操作已经完成，直接返回
-                return;
-            }
 
-            // 查询setmeal的数据
-            setmealVO = setmealMapper.findById(shoppingCartDTO.getSetmealId());
-            // 如果是第一次添加到购物车，设置数据
-            shoppingCart.setSetmealId(setmealVO.getId())
-                    .setName(setmealVO.getName())
-                    .setAmount(setmealVO.getPrice())
-                    .setImage(setmealVO.getImage());
-        }else{
-            DishVO dishVO = DishVO.builder().build();
-            ShoppingCart updateShoppingCart = ShoppingCart.builder().build();
-            if((updateShoppingCart = shoppingCartMapping.findDataById(null,shoppingCartDTO.getDishId(),userId)) != null){
+        List<ShoppingCart> list = shoppingCartMapping.findCart(shoppingCart);
 
-                // 得到需要更新的购物车数据
-                ShoppingCart updateData = getUpdateShoppingCart(updateShoppingCart, userId);
-                // 需要修改数量和该套餐在购物车中总价
-                shoppingCartMapping.updateShoppingCart(updateData);
-                return;
-            }
-
-            dishVO = dishMapper.findById(shoppingCartDTO.getDishId());
-            shoppingCart.setDishId(shoppingCartDTO.getDishId())
-                    .setName(dishVO.getName())
-                    .setAmount(dishVO.getPrice())
-                    .setImage(dishVO.getImage());
+        // 判断购物车中是否已经有该商品
+        if(list != null && !list.isEmpty()) {
+            ShoppingCart cart = list.get(0);
+            Integer oldNumber = cart.getNumber();
+            BigDecimal singlePrice = cart.getAmount().divide(BigDecimal.valueOf(oldNumber), 2, RoundingMode.HALF_UP);
+            cart.setUserId(userId);
+            cart.setNumber(oldNumber + 1);
+            cart.setAmount(singlePrice.multiply(BigDecimal.valueOf(oldNumber+1)));
+            shoppingCartMapping.updateCart(cart);
+            // 添加完直接返回即可
+            return;
         }
 
-        // 最后无论是查Setmeal还是Dish其实都是使用这些方法
-        shoppingCart.setCreateTime(LocalDateTime.now());
+        // 如果没有则直接添加，但是要判断是哪一种是setmeal还是dish
+        if(shoppingCart.getSetmealId() != null && shoppingCart.getSetmealId() > 0) {
+            SetmealVO setmealVO = setmealMapper.findById(shoppingCart.getSetmealId());
+            shoppingCart.setImage(setmealVO.getImage())
+                    .setName(setmealVO.getName())
+                    .setCreateTime(LocalDateTime.now());
+        }else if(shoppingCart.getDishId() != null && shoppingCart.getDishId() > 0) {
+            DishVO dishVO = dishMapper.findById(shoppingCart.getDishId());
+            // 不需要考虑设置菜品口味，如果有，再BeanCopy的时候就已经set了，如果没有那么就让它为null即可
+            shoppingCart.setImage(dishVO.getImage())
+                    .setName(dishVO.getName())
+                    .setCreateTime(LocalDateTime.now())
+                    // 设置初始价格
+                    .setAmount(dishVO.getPrice());
+        }
+        // 最后添加
+        shoppingCart.setNumber(1);
+        // 设置初始数量
         shoppingCartMapping.addShoppingCart(shoppingCart);
     }
 
 
-    public ShoppingCart getUpdateShoppingCart(ShoppingCart updateShoppingCart,Long userId) {
-        Integer setmealNumber = updateShoppingCart.getNumber();
+    // TODO
+//    // NOTICE
+//    @Override
+//    @Transactional
+//    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+//        // 现构建好购物车对象
+//        ShoppingCart shoppingCart = ShoppingCart.builder().build();
+//        // 先将userId赋值给shoppingCart
+//        Long userId = BaseContext.getCurrentId().get(JwtClaimsConstant.USER_ID);
+//        shoppingCart.setUserId(userId);
+//        // 判断添加进购物车的对象是Setmeal还是Dish,由此获取Setmeal还是Dish
+//        if (shoppingCartDTO.getSetmealId() != null && shoppingCartDTO.getSetmealId() > 0) {
+//            // select:判断该用户是否是第一次添加，如果是同样的商品，update:只需要修改购物车中的商品，将数量加一以及修改价格
+//            SetmealVO setmealVO = SetmealVO.builder().build();
+//            ShoppingCart updateShoppingCart = ShoppingCart.builder().build();
+//            if ((updateShoppingCart = shoppingCartMapping.findDataById(shoppingCartDTO.getSetmealId(), null, userId)) != null) {
+//
+//                // 得到需要更新的购物车数据
+//                ShoppingCart updateData = getUpdateShoppingCart(updateShoppingCart, userId);
+//                // 需要修改数量和该套餐在购物车中总价
+//                shoppingCartMapping.updateShoppingCart(updateData);
+//                // 对数据库的操作已经完成，直接返回
+//                return;
+//            }
+//
+//            // 查询setmeal的数据
+//            setmealVO = setmealMapper.findById(shoppingCartDTO.getSetmealId());
+//            // 如果是第一次添加到购物车，设置数据
+//            shoppingCart.setSetmealId(setmealVO.getId())
+//                    .setName(setmealVO.getName())
+//                    .setAmount(setmealVO.getPrice())
+//                    .setImage(setmealVO.getImage());
+//        } else {
+//            DishVO dishVO = DishVO.builder().build();
+//            ShoppingCart updateShoppingCart = ShoppingCart.builder().build();
+//            if ((updateShoppingCart = shoppingCartMapping.findDataById(null, shoppingCartDTO.getDishId(), userId)) != null) {
+//
+//                // 得到需要更新的购物车数据
+//                ShoppingCart updateData = getUpdateShoppingCart(updateShoppingCart, userId);
+//                // 需要修改数量和该套餐在购物车中总价
+//                shoppingCartMapping.updateShoppingCart(updateData);
+//                return;
+//            }
+//
+//            dishVO = dishMapper.findById(shoppingCartDTO.getDishId());
+//            shoppingCart.setDishId(shoppingCartDTO.getDishId())
+//                    .setName(dishVO.getName())
+//                    .setAmount(dishVO.getPrice())
+//                    .setImage(dishVO.getImage());
+//        }
+//
+//        // 最后无论是查Setmeal还是Dish其实都是使用这些方法
+//        shoppingCart.setCreateTime(LocalDateTime.now());
+//        // 初始值都为1份
+//        shoppingCart.setNumber(1);
+//        shoppingCartMapping.addShoppingCart(shoppingCart);
+//    }
+
+    public ShoppingCart getUpdateShoppingCart(ShoppingCart updateShoppingCart, Long userId) {
+        Integer oldNumber = updateShoppingCart.getNumber();
+        int newNumber = oldNumber + 1;
         // 商品总价
         BigDecimal amount = updateShoppingCart.getAmount();
         // 商品单价
-        BigDecimal setmealSinglePrice = amount.divide(BigDecimal.valueOf(setmealNumber),2, RoundingMode.HALF_UP);
-        updateShoppingCart.setNumber(setmealNumber + 1);
+        BigDecimal setmealSinglePrice = amount.divide(BigDecimal.valueOf(oldNumber), 2, RoundingMode.HALF_UP);
+        updateShoppingCart.setNumber(newNumber);
         updateShoppingCart.setUserId(userId);
-        updateShoppingCart.setAmount(setmealSinglePrice.multiply(BigDecimal.valueOf(setmealNumber)));
+        updateShoppingCart.setAmount(setmealSinglePrice.multiply(BigDecimal.valueOf(newNumber)));
         return updateShoppingCart;
     }
 }
