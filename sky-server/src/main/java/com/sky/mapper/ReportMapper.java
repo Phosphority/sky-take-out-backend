0f9083d6e5ce366985e8c7504ef61b8e20a3bc56
Phosphophllite <2891740789@qq.com>
2025-05-06 20:13:31 +0800

    已经完成了营业额统计相关接口

--- sky-server/src/main/java/com/sky/mapper/ReportMapper.java
+++ sky-server/src/main/java/com/sky/mapper/ReportMapper.java
@@ -2,7 +2,10 @@ package com.sky.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Map;
+
 @Mapper
 public interface ReportMapper {
 
+    Double sumByMap(Map<String, Object> map);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sky.service.impl;
 
+import com.sky.entity.Orders;
 import com.sky.mapper.ReportMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
@@ -8,8 +9,9 @@ import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.*;
 
 @Service
 public class ReportServiceImpl implements ReportService {
@@ -20,16 +22,32 @@ public class ReportServiceImpl implements ReportService {
 
     @Override
     public TurnoverReportVO turnoverStatistics(LocalDate begin, LocalDate end) {
+
+        // 得到日期
         List<LocalDate> dateList = new ArrayList<>();
         dateList.add(begin);
-
         while (!(begin.equals(end))) {
             begin = begin.plusDays(1);
             dateList.add(begin);
         }
 
+        // 得到每天的所对应的营业额
+        List<Double> totalList = new ArrayList<>();
+        while (!dateList.isEmpty()) {
+            LocalDateTime beginTime = LocalDateTime.of(begin, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(end, LocalTime.MAX);
+            Map<String, Object> map = new HashMap<>();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double total = reportMapper.sumByMap(map);
+            totalList.add(total);
+        }
+
+        // 返回封装好的数据
         return TurnoverReportVO
                 .builder()
+                .turnoverList(StringUtils.join())
                 .dateList(StringUtils.collectionToCommaDelimitedString(dateList))
                 .build();
     }
