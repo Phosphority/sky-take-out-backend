edb169270afa4b8877326584f974a4505296e8b5
Phosphophllite <2891740789@qq.com>
2025-05-21 22:26:13 +0800

    修改了Result相关的配置

--- sky-common/src/main/java/com/sky/result/Result.java
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -1,6 +1,9 @@
 package com.sky.result;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 
@@ -9,6 +12,9 @@ import java.io.Serializable;
  * @param <T>
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Accessors(chain = true)
 public class Result<T> implements Serializable {
 
     private Integer code; //编码：1成功，0和其它数字为失败
@@ -29,10 +35,11 @@ public class Result<T> implements Serializable {
     }
 
     public static <T> Result<T> error(String msg) {
-        Result result = new Result();
-        result.msg = msg;
-        result.code = 0;
-        return result;
+        return new Result(0,msg);
     }
 
+    public Result(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
 }
